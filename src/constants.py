d79e9d84360ab189aad6e14e3b0c8f9c67bb9b0e
Adam Smith <liversmiles@gmail.com>
2022-02-18 14:09:22 -0500

    refactored the program
    refactored the program. created src folder to give it a cleaner look. borke everything down and converted it to functions and a constant varible file. added requirements file

---
+++ src/constants.py
@@ -0,0 +1,141 @@
+# Recipes
+blt = {'bacon': 1, 'leaf lettuce': 1, 'tomato': 2}
+steak = {'steak': 1, 'side vegetable': 1}
+burgers = {'ground beef': 1, 'side vegetable': 1, 'cheddar': 1}
+bean_dish = {'cannelini beans': 2, 'kidney beans': 1, 'baby spinach': 1, 'smoked sausage': 1, 'diced tomatoes': 1,
+             'garlic': 1, 'chicken boullion': 1}
+enchiladas = {'corn tortillas': 1, 'ground beef': 1, 'black beans': 1, 'enchilada sauce': 1, 'diced chiles': 1,
+              'monterrey jack': 1, 'cilantro': 1, 'jalapeno': 1}
+black_bean_soup = {'black beans': 1, 'salsa': 1, 'garlic': 1, 'chicken boullion': 1}
+bbq = {'ground beef': 1, 'chicken gumbo soup': 1}
+potato_soup = {'potatoes': 6, 'celery': 1, 'onion': 1, 'bacon': 1, 'ham': 1}
+pot_pie = {'chicken breast': 1, 'carrots': 1, 'peas': 1, 'potatoes': 3, 'onion': 1, 'chicken boullion': 1}
+steak_stir_fry = {'garlic': 1, 'sirloin': 1, 'onion': 1, 'cilantro': 1, 'soy sauce': 1, 'broccoli': 1, 'snow peas': 1}
+blackened_chicken = {'chicken breast': 1, 'chicken boullion': 1, 'limes': 2, 'cilantro': 1, 'avocado': 1,
+                     'yogurt(greek)': 1}
+french_onion_soup = {'onion': 3, 'beef boullion': 1, 'worchestershire sauce': 1, 'swiss': 1}
+white_chicken_chili = {'chicken breast': 1, 'onion': 1, 'garlic': 1, 'chicken boullion': 1, 'great northern beans': 2,
+                       'diced chiles': 2, 'corn': 1, 'cilantro': 1, 'cream cheese': 1, 'whipping cream': 1}
+zuppa_toscana_soup = {'bacon': 1, 'italian sausage': 1, 'garlic': 1, 'onion': 1, 'chicken boullion': 1, 'potatoes': 3,
+                      'baby spinach': 1, 'whipping cream': 1}
+stuffed_pepper_soup = {'ground beef': 1, 'onion': 1, 'garlic': 1, 'beef boullion': 1, 'tomato sauce(29oz)': 2,
+                       'green peppers': 4, 'pre-cooked quinoa': 1, 'cheddar': 1}
+fish = {'fish(pick)': 1, 'side vegetable': 1}
+chicken = {'chicken(pick)': 1, 'side vegetable': 1}
+pasta = {'noodles': 1, 'ground beef': 1, 'pasta sauce': 1, 'mozzarella': 1}
+beef_stew = {'stew meat': 2, 'beef boullion': 1, 'onion': 1, 'carrot': 1, 'peas': 1, 'potatoes': 3}
+tacos = {'ground beef': 1, 'tomatoes': 2, 'cilantro': 1, 'cheddar': 1, 'jalapeno': 1, 'taco shells': 1}
+burrito_bowls = {'flank steak': 1, 'garlic': 1, 'limes': 4, 'soy sauce': 1, 'tomatoes': 2, 'cilantro': 1,
+                 'black beans': 1, 'cheddar': 1, 'jalapeno': 1, 'avocado': 4}
+butter_chicken = {'chicken breast': 1, 'lemons': 1, 'yogurt(greek)': 1, 'garlic': 1, 'jalapeno': 1,
+                  'tomato sauce(8oz)': 1, 'whipping cream': 1, 'cilantro': 1}
+pork_chops = {'pork chops': 1, 'side vegetable': 1}
+steak_fajitas = {'sirloin': 1, 'red peppers': 1, 'green peppers': 1, 'yellow peppers': 1, 'onion': 1, 'fajita mix': 1,
+                 'corn tortillas': 1}
+chicken_fajitas = {'chicken breast': 1, 'red peppers': 1, 'green peppers': 1, 'yellow peppers': 1, 'onion': 1,
+                   'fajita mix': 1, 'corn tortillas': 1}
+chicken_wild_rice_soup = {'chicken breast': 1, 'carrots': 1, 'onion': 1, 'celery': 1, 'chicken boullion': 1,
+                          'wild rice': 1, 'whipping cream': 1}
+gumbo = {'smoked sausage': 2, 'onion': 1, 'green peppers': 2, 'celery': 1, 'garlic': 1, 'tomato paste': 1,
+         'diced tomatoes': 1, 'green onions': 1, 'chicken boullion': 1, 'okra': 1, 'shrimp': 1}
+basil_chicken_stir_fry = {'chicken thigh': 1, 'green peppers': 2, 'jalapeno': 1, 'shallots': 1, 'basil': 1,
+                          'fish sauce': 1}
+lettuce_wraps = {'ground chicken': 1, 'garlic': 1, 'onion': 1, 'hoisin sauce': 1, 'ginger': 1, 'water chestnuts': 1,
+                 'green onions': 1, 'head lettuce': 1}
+pizza = {'pizza crust': 1, 'pizza sauce': 1, 'mozzarella': 1, 'pepperoni': 1, 'shitake': 2}
+
+# List of Recipes
+recipeList = {'blt': blt, 'steak': steak, 'burgers': burgers, 'bean_dish': bean_dish, 'enchiladas': enchiladas,
+              'black_bean_soup': black_bean_soup, 'potato_soup': potato_soup, 'french_onion_soup': french_onion_soup,
+              'zuppa_toscana_soup': zuppa_toscana_soup, 'stuffed_pepper_soup': stuffed_pepper_soup,
+              'chicken_wild_rice_soup': chicken_wild_rice_soup, 'pork_chops': pork_chops, 'chicken': chicken,
+              'fish': fish, 'steak_fajitas': steak_fajitas, 'chicken_fajitas': chicken_fajitas, 'tacos': tacos,
+              'burrito_bowls': burrito_bowls, 'bbq': bbq, 'lettuce_wraps': lettuce_wraps, 'pot_pie': pot_pie,
+              'white_chicken_chili': white_chicken_chili, 'pasta': pasta, 'steak_stir_fry': steak_stir_fry,
+              'beef_stew': beef_stew, 'gumbo': gumbo, 'basil_chicken_stir_fry': basil_chicken_stir_fry, 'pizza': pizza,
+              'blackened_chicken': blackened_chicken}
+
+# Staples
+mini_cukes = {'mini cukes': 1}
+landjaegers = {'landjaegers': 1}
+summer_sausage = {'summer sausage': 1}
+jalapeno_meunster = {'jalapeno meunster': 1}
+feta = {'feta': 1}
+waffles = {'waffles': 1}
+coffee = {'coffee': 1}
+cheddar = {'cheddar': 1}
+swiss = {'swiss': 1}
+mozzarella = {'mozzarella': 1}
+colby = {'colby': 1}
+baby_swiss = {'baby swiss': 1}
+spreadable_cheese = {'spreadable cheese': 1}
+parmesean_block = {'parmesean(block)': 1}
+parmesean_grated = {'parmesean(grated)': 1}
+orange_juice = {'orange juice': 1}
+milk = {'milk': 1}
+eggs = {'eggs': 1}
+butter = {'butter': 1}
+cherubs = {'cherubs': 1}
+green_apples = {'green apples': 5}
+cottage_cheese = {'cottage cheese': 1}
+yogurt_tub = {'yogurt(tub)': 1}
+yogurt_pouch = {'yogurt(pouch)': 1}
+yogurt_greek = {'yogurt(greek)': 1}
+yogurt_drink = {'yogurt(drink)': 1}
+strawberries = {'strawberries': 1}
+raspberries = {'raspberries': 1}
+grapes = {'grapes': 1}
+lemons = {'lemons': 1}
+limes = {'limes': 1}
+bananas = {'bananas': 1}
+sour_cream = {'sour cream': 1}
+creamer = {'creamer': 1}
+hot_dogs = {'hot dogs': 1}
+bacon = {'bacon': 1}
+sausage = {'sausage': 1}
+
+# List of Staples
+staplesList = {'strawberries': strawberries, 'raspberries': raspberries, 'grapes': grapes, 'lemons': lemons,
+               'bananas': bananas, 'green_apples': green_apples, 'limes': limes, 'mini_cukes': mini_cukes,
+               'cherubs': cherubs, 'hot_dogs': hot_dogs, 'bacon': bacon, 'sausage': sausage, 'landjaegers': landjaegers,
+               'summer_sausage': summer_sausage, 'jalapeno_meunster': jalapeno_meunster, 'feta': feta,
+               'cheddar': cheddar, 'swiss': swiss, 'mozzarella': mozzarella, 'colby': colby, 'baby_swiss': baby_swiss,
+               'spreadable_cheese': spreadable_cheese, 'parmesean_block': parmesean_block,
+               'parmesean_grated': parmesean_grated, 'milk': milk, 'eggs': eggs, 'butter': butter,
+               'yogurt_tub': yogurt_tub, 'yogurt_pouch': yogurt_pouch, 'yogurt_drink': yogurt_drink,
+               'yogurt_greek': yogurt_greek, 'sour_cream': sour_cream, 'creamer': creamer,
+               'cottage_cheese': cottage_cheese, 'orange_juice': orange_juice, 'waffles': waffles, 'coffee': coffee}
+
+# Departments
+fruit = ['strawberries', 'raspberries', 'grapes', 'lemons', 'limes', 'green apples', 'bananas']
+produce1 = ['avocado', 'mini cukes', 'baby spinach', 'ginger', 'celery', 'carrots', 'jalapeno', 'green peppers',
+            'yellow peppers', 'red peppers', 'basil']
+produce2 = ['side vegetable', 'green onion', 'onion', 'potatoes', 'cherubs', 'tomatoes', 'leaf lettuce', 'head lettuce',
+            'broccoli', 'cauliflower']
+produce3 = ['garlic', 'cilantro', 'snow peas', 'okra', 'shallots', 'shitake', 'mushrooms']
+meat1 = ['ground chicken', 'chicken breast', 'chicken thigh', 'chicken(pick)', 'bacon', 'smoked sausage', 'hot dogs',
+         'sausage']
+meat2 = ['steak', 'pork chops', 'sirloin', 'flank steak', 'ground beef', 'italian sausage', 'stew meat', 'ham']
+meat3 = ['pepperoni', 'shrimp', 'fish(pick)', 'landjaegers', 'summer sausage']
+dairy1 = ['cream cheese', 'butter']
+dairy2 = ['cheddar', 'swiss', 'monterrey jack', 'mozzarella', 'jalapeno meunster', 'feta', 'colby', 'baby swiss',
+          'spreadable cheese', 'parmesean(block)', 'parmesean(grated)']
+dairy3 = ['whipping cream', 'cottage cheese', 'sour cream', 'creamer', 'yogurt(tub)', 'yogurt(pouch)', 'yogurt(drink)',
+          'yogurt(greek)', 'milk', 'eggs', 'orange juice']
+frozen = ['peas', 'waffles']
+gluten_free = ['noodles', 'pizza crust']
+coffee_spices = ['coffee']
+mexican = ['salsa', 'diced chiles', 'fajita mix', 'taco shells']
+asian = ['fish sauce', 'hoisin sauce', 'water chestnuts', 'soy sauce']
+soup = ['chicken boullion', 'beef boullion', 'chicken gumbo soup']
+noodles_sauce = ['pasta sauce', 'pizza sauce']
+beans_aisle = ['cannelini beans', 'kidney beans', 'black beans', 'great northern beans']
+rice_tomatoes = ['wild rice', 'corn', 'tomato sauce(29oz)', 'pre-cooked quinoa', 'tomato paste', 'diced tomatoes']
+condiments = ['worchestershire sauce']
+
+produce_list = fruit + produce1 + produce2 + produce3
+meat_list = meat1 + meat2 + meat3
+dairy_list = dairy1 + dairy2 + dairy3
+frozen_list = frozen
+gluten_free_list = gluten_free
+dry_food_list = coffee_spices + mexican + asian + soup + noodles_sauce + beans_aisle + rice_tomatoes
